32de8942f51e95de454e05ad00c99070b1667dd6
zhangchi1994 <zccc@umich.edu>
2017-06-14 15:43:32 +0800

    removed .class again fixed insert

--- src/ve450/ruix/sql_connection.java
+++ src/ve450/ruix/sql_connection.java
@@ -22,38 +22,39 @@ public class sql_connection {
 	public void Insert(String id, String product_name, String manufacturer, String time, String size) {
 		try {
 			Statement st = con.createStatement();
-			
+
 			String sql;
-			//if(id.equals("")){
-				sql = "INSERT INTO Equipment"
-						+ "(name, manufacturer, date_of_birth, last_maintenance_date, status, ssize, dad_id) VALUES("
-						+ product_name + "," + manufacturer + "," + time + "," + "'0'" + "," + size + "," + "0";
-			//}
+			// if(id.equals("")){
+			sql = "INSERT INTO Equipment"
+					+ "(name, manufacturer, date_of_birth, last_maintenance_date, status, ssize, dad_id) VALUES('"
+					+ product_name + "','" + manufacturer + "','" + time + "','" + time + "','" + "0" + "','" + size
+					+ "'," + "0)";
+			// }
 			st.executeQuery(sql);
 			st.close();
 		} catch (Exception ee) {
 			System.out.print("error in updateInfo");
 		}
 	}
-		
-		 public boolean logIN(String username, String password)
-	     {		
-			 boolean find_or_not = false;
-	         try 
-	          {
-	            Class.forName("org.postgresql.Driver").newInstance();
-	            
-	            String url = "jdbc:postgresql://localhost:5432/example_db" ;
-	            //String url = "jdbc:postgresql://ve450postgresql.nat123.cc:44966/example_db" ;
-	            Connection con = DriverManager.getConnection(url, "postgres" , "123456");           
-	            //Connection con = DriverManager.getConnection(url, "postgres" , "password");
-	            Statement st = con.createStatement();
-	            String sql = "select * from users where user_id="+username;
-	            //String sql = "select * from child where equipment_id="+Item_id;
-	            ResultSet rs = st.executeQuery(sql);
-	            if(!rs.next()){
-	            	System.out.println("hi_1");
-	            	find_or_not = false;
+
+	public boolean logIN(String username, String password) {
+		boolean find_or_not = false;
+		try {
+			Class.forName("org.postgresql.Driver").newInstance();
+
+			String url = "jdbc:postgresql://localhost:5432/example_db";
+			// String url =
+			// "jdbc:postgresql://ve450postgresql.nat123.cc:44966/example_db" ;
+			Connection con = DriverManager.getConnection(url, "postgres", "123456");
+			// Connection con = DriverManager.getConnection(url, "postgres" ,
+			// "password");
+			Statement st = con.createStatement();
+			String sql = "select * from users where user_id=" + username;
+			// String sql = "select * from child where equipment_id="+Item_id;
+			ResultSet rs = st.executeQuery(sql);
+			if (!rs.next()) {
+				System.out.println("hi_1");
+				find_or_not = false;
 
 				/*
 				 * if (rs.next()) { byebye[0] = rs.getString("equipment_id");
