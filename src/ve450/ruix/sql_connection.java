759d522eb34aee8c3c20f441c82ed1c68526cbbc
zhangchi1994 <zccc@umich.edu>
2017-06-14 21:27:54 +0800

    added return id in insert

--- src/ve450/ruix/sql_connection.java
+++ src/ve450/ruix/sql_connection.java
@@ -19,7 +19,8 @@ public class sql_connection {
 	}
 
 	// TODO: this currently does not work because con has been closed!
-	public void Insert(String product_name, String manufacturer, String time, String size) {
+	public String Insert(String product_name, String manufacturer, String time, String size) {
+		String returnId = "Haha you fail";
 		try {
 			Class.forName("org.postgresql.Driver").newInstance();
 			String url = "jdbc:postgresql://localhost:5432/example_db";
@@ -29,6 +30,52 @@ public class sql_connection {
 			Statement st = con.createStatement();
 
 			String sql;
+			sql = "INSERT INTO Equipment (name, manufacturer, date_of_birth, last_maintenance_date, status, ssize, dad_id) VALUES ('"
+					+ product_name + "','" + manufacturer + "','" + time + "','" + time + "','" + "0" + "','" + size
+					+ "'," + "0)";
+			// sql = "INSERT INTO Equipment (name, manufacturer, date_of_birth,
+			// last_maintenance_date, status, ssize, dad_id)"
+			// + " VALUES ('pn', 'manu1', '1989-08-18', '1989-08-18','0',
+			// '17kg',0)";
+			// }
+			st.executeUpdate(sql);
+			ResultSet generatedKeys = st.getGeneratedKeys();
+	        if (generatedKeys.next()) {
+	             returnId = generatedKeys.getString(1);
+	             System.out.println("The id is " + returnId);
+	        }
+			System.out.println("Someone Upload");
+			st.close();
+			con.close();
+			
+		} catch (Exception ee) {
+			System.out.print("error in updateInfo");
+		}
+		return returnId;
+	}
+
+	/*public void Read(String id) {
+		try {
+			Class.forName("org.postgresql.Driver").newInstance();
+			String url = "jdbc:postgresql://localhost:5432/example_db";
+			// String url =
+			// "jdbc:postgresql://ve450postgresql.nat123.cc:44966/example_db" ;
+			Connection con = DriverManager.getConnection(url, "postgres", "123456");
+			Statement st = con.createStatement();
+
+			String sql;
+			if(rs.next()){
+				
+				  if (rs.next()) { byebye[0] = rs.getString("equipment_id");
+				  byebye[1] = rs.getString("name");
+				  byebye[2] = rs.getString("date_of_birth"); byebye[3] =
+				 rs.getString("last_maintenance_date"); if(rs.getInt("dad_id")
+				  == 0){ byebye[4] = "NULL"; byebye[5] = "NULL"; } else {
+				  byebye[4] = rs.getString("dad_id"); byebye[5] =
+				  "http://localhost:8080/VE450/Info_"+byebye[4]+".jsp"; }
+				  System.out.println(byebye[4]);
+				 
+			}
 			sql = "INSERT INTO Equipment (name, manufacturer, date_of_birth, last_maintenance_date, status, ssize, dad_id) VALUES ('" 
 					+ product_name + "','" + manufacturer + "','" + time + "','" + time 
 					+ "','" + "0" + "','" + size+ "'," + "0)";
@@ -42,7 +89,7 @@ public class sql_connection {
 		} catch (Exception ee) {
 			System.out.print("error in updateInfo");
 		}
-	}
+	}*/
 
 	public boolean logIN(String username, String password) {
 		boolean find_or_not = false;
@@ -57,22 +104,12 @@ public class sql_connection {
 			// "password");
 			Statement st = con.createStatement();
 			String sql = "select * from users where user_id=" + username;
-			// String sql = "select * from child where equipment_id="+Item_id;
+			
 			ResultSet rs = st.executeQuery(sql);
 			if (!rs.next()) {
 				System.out.println("hi_1");
 				find_or_not = false;
 
-				/*
-				 * if (rs.next()) { byebye[0] = rs.getString("equipment_id");
-				 * byebye[1] = rs.getString("name"); byebye[2] =
-				 * rs.getString("date_of_birth"); byebye[3] =
-				 * rs.getString("last_maintenance_date"); if(rs.getInt("dad_id")
-				 * == 0){ byebye[4] = "NULL"; byebye[5] = "NULL"; } else {
-				 * byebye[4] = rs.getString("dad_id"); byebye[5] =
-				 * "http://localhost:8080/VE450/Info_"+byebye[4]+".jsp"; }
-				 * System.out.println(byebye[4]);
-				 */
 			} else {
 				if (rs.getString("password").equals(password))
 					find_or_not = true;
@@ -81,7 +118,7 @@ public class sql_connection {
 			}
 			rs.close();
 			st.close();
-			// con.close(); // TODO: remove this line after log out is
+			con.close(); // TODO: remove this line after log out is
 			// implemented
 			return (find_or_not);
 
