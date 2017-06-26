560bc97409c775f4957e511bc2f6d10809e23e77
ruix95 <shery950712@outlook.com>
2017-06-26 15:40:58 +0800

    自动打印，自己改一下，请

--- WebContent/js/fatherPage.js
+++ WebContent/js/fatherPage.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 	//alert(sessionStorage.getItem("qr_id_data"));
 	$("#id").text(sessionStorage.getItem("qr_id_data"));
-	document.getElementById("pic_id").src = "http://localhost:8080/VE450/pic/" + sessionStorage.getItem("qr_id_data") + ".png";
+	document.getElementById("pic_id").src = "http://4607ad2e.nat123.cc/VE450/pic/" + sessionStorage.getItem("qr_id_data") + ".png";
 	//alert(document.getElementById("pic_id").src);
 	//$("#name").load("http://4607ad2e.nat123.cc/VE450/testINPUT.html?id=welcometext");
 	/*$.get("testINPUT.html?id=welcometext",function(data){
--- WebContent/js/webcam.js
+++ WebContent/js/webcam.js
@@ -21,7 +21,65 @@ function CatchCode() {
     	name : filename 
     	}, function(responseText){  
     		jQuery(function(){
-			jQuery('#output').qrcode(responseText);
+    			jQuery('#output').qrcode(responseText);
 		})
+    		var LODOP; 
+    		LODOP=getLodop();  
+    		LODOP.PRINT_INITA(0,0,522,333,"打印控件功能演示_Lodop功能_自定义纸张1");	
+    		LODOP.SET_PRINT_PAGESIZE(0,document.getElementById('W1').value,document.getElementById('H1').value,"A4");
+    		AddPrintContent(document.getElementById("getval").value);
+    		//LODOP.PRINT();	
+    	  	LODOP.PREVIEW();
+    		function AddPrintContent(strCode) {		
+    			LODOP.ADD_PRINT_BARCODE("0.2cm","0.2cm","2.8cm","2.8cm","QRCode",strCode);
+    			//LODOP.SET_PRINT_STYLEA(0,"Angle",-90);
+    			
+    			
+    		};
+    		function getSelectedPrintIndex(){
+    			if (document.getElementById("Radio2").checked) 
+    			return document.getElementById("PrinterList").value;
+    			else return -1; 		
+    		};
+    		function getSelectedPageSize(){
+    			if (document.getElementById("Radio4").checked) 
+    			return document.getElementById("PagSizeList").value;
+    			else return ""; 		
+    		};	
+    		function CreatePrinterList(){
+    		    if (document.getElementById('PrinterList').innerHTML!="") return;
+    			LODOP=getLodop(); 
+    			var iPrinterCount=LODOP.GET_PRINTER_COUNT();
+    			for(var i=0;i<iPrinterCount;i++){
+
+    	   			var option=document.createElement('option');
+    	   			option.innerHTML=LODOP.GET_PRINTER_NAME(i);
+    	   			option.value=i;
+    				document.getElementById('PrinterList').appendChild(option);
+    			};	
+    		};
+    		function clearPageListChild(){
+    		   var PagSizeList =document.getElementById('PagSizeList'); 
+    		   while(PagSizeList.childNodes.length>0){
+    	  		   var children = PagSizeList.childNodes;	
+    		  		 for(i=0;i<children.length;i++){		
+    				PagSizeList.removeChild(children[i]);	
+    		  	  };	   
+    		   };	   
+    		}
+    		function CreatePagSizeList(){
+    		   LODOP=getLodop(); 
+    		   clearPageListChild();
+    		   var strPageSizeList=LODOP.GET_PAGESIZES_LIST(getSelectedPrintIndex(),"\n");
+    		   var Options=new Array(); 
+    	 	   Options=strPageSizeList.split("\n");       
+    		   for (i in Options)    
+    		   {    
+    		     var option=document.createElement('option');   
+    			 option.innerHTML=Options[i];
+    			 option.value=Options[i];
+    	   		 document.getElementById('PagSizeList').appendChild(option);
+    		   }  
+    		}	
     });  
  }  
